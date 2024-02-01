e8c2ab4792e7463659174ba8e9fb4ed90cd4aa39
lucio albornoz <lucioga32@gmail.com>
2024-01-31 22:33:26 -0300

    Ejercicio 1 resulto

--- Ejercicios-Extras/src/Main.java
+++ Ejercicios-Extras/src/Main.java
@@ -1,5 +1,25 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa un numero: ");
+        int numero = sc.nextInt();
+        if (esPrimos(numero,2)){
+            System.out.println(numero + " Es un numero primo");
+        }else {
+            System.out.println(numero + " No es un numero primo");
+        }
+    }
+    public static boolean esPrimos(int nro,int divisor){
+
+        if (nro == divisor){
+            return true;
+        } else if (nro % divisor ==0 || nro < 2) {
+            return false;
+        }
+        return esPrimos(nro,divisor + 1);
     }
 }
